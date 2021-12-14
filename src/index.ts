737e0c4c9768225e26b8377a653beb61935cc2e6
Undefined K <favoursmoe@gmail.com>
2021-12-14 21:47:27 +0800

    Remove comments

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,5 @@ import { HumidifierAccessory } from './HumidifierAccessory';
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
-  // api.registerAccessory(ACCESSORY_NAME, NaderAccessory);
   api.registerAccessory('MiHumidifier2', HumidifierAccessory);
 };
